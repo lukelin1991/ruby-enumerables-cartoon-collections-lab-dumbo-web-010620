cb7a8fc18a997a4d8bd51efa4552cd1cdf4d89d8
Luke Lin <lukelin1991@yahoo.com>
2019-12-10 18:58:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(elements)# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_elements)# code an argument here
   # Your code here
 end
 
