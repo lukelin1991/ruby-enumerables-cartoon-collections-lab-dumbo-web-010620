1476e27ccef7150cd7a6ce7600dabb6c3d106e32
Luke Lin <lukelin1991@yahoo.com>
2019-12-10 19:04:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,6 +4,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |name, index|
     index += 1
     puts "#{index}. #{name}"
+    binding.pry
   end
   # Your code here
 end
